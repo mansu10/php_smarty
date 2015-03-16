be211a356cdaf0699ebe931b3cb66d68a5fbc4b3
rain <lixinyu0476@163.com>
2015-03-16 17:55:59 +0800

    init project

---
+++ project/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta chartset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html">
+		<title>PHP程序</title>
+
+	</head>
+	<body>
+		<?php 
+			echo "Hello World";
+		?>	
+	</body>
+</html>
